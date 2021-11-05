30b38de32433848d008861178cc18ff551341d5f
erm0es <emelie.rosendahl@hotmail.se>
2021-11-05 20:04:07 +0100

    menu phone

--- Script.js
+++ Script.js
@@ -1,1 +1,18 @@
 console.log ("Script is running..");
+
+const headerMenuUL = document.querySelector('.headerMenuUL');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+headerMenuUL.style.display = 'flex';
+headerMenuUL.style.top = '0';
+
+}
+
+function close(){
+  headerMenuUL.style.top = '-100%';
+}
